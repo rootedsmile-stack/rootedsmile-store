5cfe076351219410eadac99a7857f41d4a6f549f
rootedsmile-stack <geetsupra50@gmail.com>
2025-10-20 21:48:10 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,6 @@
-
 import adapter from '@sveltejs/adapter-cloudflare';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
 const config = {
-  preprocess: vitePreprocess(),
   kit: {
     adapter: adapter(),
     csrf: { checkOrigin: false }
